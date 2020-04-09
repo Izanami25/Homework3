c12c2c0735b9b031080f739d29aa56a96562cea7
Izanami25 <54244062+Izanami25@users.noreply.github.com>
2020-04-09 20:41:26 +0600

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Point {
+    protected int x;
+    protected int y;
+    public Point (){
+        this.x = x;
+        this.y = y;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    int getX(){
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+}
